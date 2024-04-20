7721b59ebb71a683e09a4bf599467533acdf4c8d
masud <masudranaacb@gmail.com>
2024-04-20 15:44:12 +0600

    added delet curd operation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const About = () => {
-    const users=useLoaderData()
+    const loaderUsers=useLoaderData()
+    const [users,setUsers]=useState(loaderUsers)
+    const handelDeleteUser=id=>{
+        fetch(`http://localhost:5000/users/${id}`,{
+            method:"DELETE"
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            const removeUser=users.filter(user=>user._id!==id)
+            setUsers(removeUser)
+        })
+        
+    }
     return (
         <div>
             <h2>This is About Page</h2>
             <div>
                 {
-                    users.map(user=><p key={user._id}>{user.name}:{user.email}</p>)
+                    users.map(user=><p key={user._id}>{user.name}:{user.email} <button className="btn btn-accent" onClick={()=>handelDeleteUser(user._id)}>delete</button></p>)
                 }
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,10 @@ const Home = () => {
 
        })
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>{
+            console.log(data)
+            form.reset()
+        })
     }
     return (
         <div>
